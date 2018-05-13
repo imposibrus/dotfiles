4f7ced146a1a43937c49210c54fdb376bcad0aa5
imposibrus <imposibrus@gmail.com>
2018-05-13 18:31:07 +0300

    add oh-my-fish config

--- install-configs.sh
+++ install-configs.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.config/{fish,nvim} ~/.ssh ~/.vim/colors
 
-ROOT_FILES=(.aliases .gitconfig .tmux.conf .vimrc .config/fish/config.fish .config/nvim/init.vim .ssh/config .vim/colors/darcula.vim)
+ROOT_FILES=(.aliases .gitconfig .tmux.conf .vimrc .config/fish/config.fish .config/nvim/init.vim .config/omf/bundle .config/omf/channel .config/omf/theme .ssh/config .vim/colors/darcula.vim)
 
 for name in "${ROOT_FILES[@]}"
 do
