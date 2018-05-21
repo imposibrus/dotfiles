d4688c56d9001fbb688cfbf43859c603c8d4d26d
imposibrus <imposibrus@gmail.com>
2018-05-21 11:27:47 +0300

    create `~/.ssh/control` folder for shared ssh connections

--- install-configs.sh
+++ install-configs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/{fish,nvim,omf} ~/.ssh ~/.vim/colors
+mkdir -p ~/.config/{fish,nvim,omf} ~/.ssh/{,control} ~/.vim/colors
 
 ROOT_FILES=(.aliases .gitconfig .tmux.conf .vimrc .config/fish/config.fish .config/nvim/init.vim .config/omf/bundle .config/omf/channel .config/omf/theme .ssh/config .vim/colors/darcula.vim)
 
